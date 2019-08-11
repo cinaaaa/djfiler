c288b840f6c869db9e34cd56360b4b18e1aac5bb
sina farhadi <sina@sinas-MacBook-Pro.local>
2019-08-11 13:13:46 +0430

    Generate Files

---
+++ core/file_upload.py
@@ -0,0 +1,17 @@
+"""
+#   Sina Farhadi
+#
+#   @2019 All Rights Served
+#
+#   We Handle Upload Files Here
+#
+"""
+
+
+#Import Name Genrate Module
+from .namegen import generate_name
+
+
+
+def upload_file(*args, **kwargs):
+    pass
